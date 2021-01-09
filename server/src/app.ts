1d545be4a09b4b825719ad1c795302e3cefbb393
jooprima <jooprima@gmail.com>
2021-01-09 22:47:24 +0700

    Configure ESLint with Prettier

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-import routes from "./routes";
+import routes from './routes';
 
 const app = express();
 const PORT = 8080;
@@ -10,7 +10,7 @@ const PORT = 8080;
 const {
   MONGODB_ATLAS_USERNAME,
   MONGODB_ATLAS_PASSWORD,
-  MONGODB_ATLAS_DBNAME,
+  MONGODB_ATLAS_DBNAME
 } = process.env;
 const uri = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@cluster0.crt4x.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`;
 
@@ -19,7 +19,7 @@ const options = { useNewUrlParser: true, useUnifiedTopology: true };
 app.use(cors());
 app.use(routes);
 
-mongoose.set("useFindAndModify", true);
+mongoose.set('useFindAndModify', true);
 mongoose
   .connect(uri, options)
   .then(() => {
--- server/src/controllers/todos/index.ts
+++ server/src/controllers/todos/index.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import TodoModel from "../../models/todo";
-import { Todo } from "../../types/todo";
+import TodoModel from '../../models/todo';
+import { Todo } from '../../types/todo';
 
 export const getTodos = async (req: Request, res: Response): Promise<void> => {
   const todos: Todo[] = await TodoModel.find();
@@ -13,7 +13,7 @@ export const getTodo = async (req: Request, res: Response): Promise<void> => {
   await TodoModel.findById(req.params.id, (err, result) => {
     if (err) {
       res.status(400).json({
-        error: err,
+        error: err
       });
     } else {
       res.status(200).json({ result });
@@ -22,12 +22,12 @@ export const getTodo = async (req: Request, res: Response): Promise<void> => {
 };
 
 export const addTodo = async (req: Request, res: Response): Promise<void> => {
-  const body: Pick<Todo, "title" | "status"> = req.body;
+  const body: Pick<Todo, 'title' | 'status'> = req.body;
 
   if (!body.title || !body.status) {
     res.status(401).json({
       status: 401,
-      errorMessage: `ValidationError: Todo validation failed:title : ${body.title}, status: ${body.status}`,
+      errorMessage: `ValidationError: Todo validation failed:title : ${body.title}, status: ${body.status}`
     });
 
     return;
@@ -35,16 +35,16 @@ export const addTodo = async (req: Request, res: Response): Promise<void> => {
 
   const newTodoModel = new TodoModel({
     title: body.title,
-    status: body.status,
+    status: body.status
   });
 
   const newTodo = await newTodoModel.save();
   const updatedAllTodosAfterSave = await TodoModel.find();
 
   res.status(201).json({
-    message: "Todo successfully added!",
+    message: 'Todo successfully added!',
     addedTodo: newTodo,
-    allTodosAfterAddition: updatedAllTodosAfterSave,
+    allTodosAfterAddition: updatedAllTodosAfterSave
   });
 };
 
@@ -54,13 +54,13 @@ export const updateTodo = async (
 ): Promise<void> => {
   const {
     params: { id },
-    body,
+    body
   } = req;
 
   if (!body.title || !body.status || !id) {
     res.status(401).json({
       status: 401,
-      errorMessage: `ValidationError: _id or required body property is not defined.`,
+      errorMessage: `ValidationError: _id or required body property is not defined.`
     });
 
     return;
@@ -72,15 +72,15 @@ export const updateTodo = async (
   if (!updateTodo) {
     res
       .status(501)
-      .json({ status: 501, errorMessage: "Edit todo failed. Not Implemented" });
+      .json({ status: 501, errorMessage: 'Edit todo failed. Not Implemented' });
 
     return;
   }
 
   res.status(200).json({
-    message: "Todo successfuly edited",
+    message: 'Todo successfuly edited',
     updatedTodo,
-    todos: updatedAllTodosAfterUpdate,
+    todos: updatedAllTodosAfterUpdate
   });
 };
 
@@ -89,13 +89,13 @@ export const removeTodo = async (
   res: Response
 ): Promise<void> => {
   const {
-    params: { id },
+    params: { id }
   } = req;
 
   if (!id) {
     res.status(401).json({
       status: 401,
-      errorMessage: `ValidationError: Params _id is not defined.`,
+      errorMessage: `ValidationError: Params _id is not defined.`
     });
 
     return;
@@ -107,15 +107,15 @@ export const removeTodo = async (
   if (!removedTodo) {
     res.status(501).json({
       status: 501,
-      errorMessage: "Remove todo failed. Not Implemented",
+      errorMessage: 'Remove todo failed. Not Implemented'
     });
 
     return;
   }
 
   res.status(200).json({
-    message: "Todo successfuly removed",
+    message: 'Todo successfuly removed',
     removedTodo,
-    todos: updatedAllTodosAfterRemove,
+    todos: updatedAllTodosAfterRemove
   });
 };
