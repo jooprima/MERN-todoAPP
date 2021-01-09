1e686244cdca7530b38461abb830d4e2675d45b6
jooprima <jooprima@gmail.com>
2021-01-09 18:08:48 +0700

    Create getTodo API Controller

--- server/src/controllers/todos/index.ts
+++ server/src/controllers/todos/index.ts
@@ -8,3 +8,15 @@ export const getTodos = async (req: Request, res: Response) => {
 
   res.status(200).json({ todos });
 };
+
+export const getTodo = async (req: Request, res: Response) => {
+  await TodoModel.findById(req.params.id, (err, result) => {
+    if (err) {
+      res.status(400).json({
+        error: err,
+      });
+    } else {
+      res.status(200).json({ result });
+    }
+  });
+};
