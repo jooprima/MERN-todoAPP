9f8d103b093dccba1db8bc95cdf7f0cb94e25257
jooprima <jooprima@gmail.com>
2021-01-10 14:36:28 +0700

    Create The Header Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Header from "components/Header";
+
 const App: React.FC = () => {
   return (
     <main className="container relative bg-darkPurple mx-auto max-w-lg p-4 box-border min-h-screen">
-      
+      <Header />
     </main>
   );
 };
