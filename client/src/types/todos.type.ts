aad1a7feeeb34043a37a54763e1215b06c58f772
jooprima <jooprima@gmail.com>
2021-01-10 12:13:07 +0700

    Create getTodos Service

---
+++ client/src/types/todos.type.ts
@@ -0,0 +1,14 @@
+import { toEditorSettings } from "typescript";
+
+export interface Todos {
+  todos: Todo[];
+}
+
+interface Todo {
+  _id: string;
+  title: string;
+  status: "completed" | "uncompleted";
+  createdAt: string;
+  updateAt: string;
+  __v: number;
+}
