32c8160d87aa92b58b016c4607ea3e2f8f2c6658
jooprima <jooprima@gmail.com>
2021-01-15 01:59:26 +0700

    Enhance Form Component UI

--- client/src/components/Form/index.tsx
+++ client/src/components/Form/index.tsx
@@ -62,6 +62,25 @@ const Form: React.FC<Props> = ({ inProp, onClose }) => {
 
   console.log("errors ??", errors);
 
+  const handleOnClose = () => {
+    reset();
+    onClose();
+  };
+
+  const placeholderStyle = classnames(
+    "text-darkPurple flex-1 bg-transparent outline-none",
+    {
+      "placeholder-red-400": errors.title,
+    }
+  );
+
+  const inputStyle = classnames(
+    "flex justify-center items-center bg-gray-200 px-4 py-2 rounded-lg box-border",
+    {
+      "bg-red-200": errors.title,
+    }
+  );
+
   return (
     <Transition in={inProp} timeout={DURATION}>
       {(state) => (
@@ -82,10 +101,7 @@ const Form: React.FC<Props> = ({ inProp, onClose }) => {
             }}
             className="fixed flex flex-col z-10 inset-x-0 rounded-t-lg p-4 h-32 bg-white"
           >
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="flex justify-center items-center bg-gray-200 px-4 py-2 rounded-lg box-border"
-            >
+            <form onSubmit={handleSubmit(onSubmit)} className={inputStyle}>
               <input
                 ref={register({
                   required: {
@@ -102,8 +118,8 @@ const Form: React.FC<Props> = ({ inProp, onClose }) => {
                   },
                 })}
                 name="title"
-                placeholder="Belajar"
-                className="text-darkPurple flex-1 bg-transparent outline-none"
+                placeholder={errors.title ? "...Ooops" : "Mari Belajar....!"}
+                className={placeholderStyle}
               />
               <input
                 ref={register}
@@ -112,17 +128,27 @@ const Form: React.FC<Props> = ({ inProp, onClose }) => {
                 className="hidden"
               />
 
-              <input
-                type="submit"
-                value="Add"
-                className="bg-transparent text-md font-bold text-darkPurple outline-none ml-1"
-              />
+              {errors.title ? (
+                <button
+                  onClick={() => reset()}
+                  className="bg-transparent text-md font-bold text-darkPurple outline-none ml-1"
+                >
+                  Reset
+                </button>
+              ) : (
+                <input
+                  type="submit"
+                  value="Add"
+                  className="bg-transparent text-md font-bold text-darkPurple outline-none ml-1"
+                />
+              )}
             </form>
             <span className="text-xs text-red-500 font-semibold tracking-wide mt-2 pl-1">
               {errors?.title?.message}
             </span>
 
             <span
+              onClick={handleOnClose}
               className="absolute transform -translate-x-1/2 -translate-y-1/2"
               style={{
                 bottom: "10px",
